5ed8b6840edb39146c4bb38de2adfce67284fbe0
陈龙 <randolph@chenlongdeMacBook-Pro.local>
2020-01-27 23:31:40 +0800

    write a java code file

---
+++ Reverse Integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while(x != 0){
+            int pop = x % 10;
+            x = x / 10;
+            if(ans > Integer.MAX_VALUE/10 || (ans == Integer.MAX_VALUE/10 && pop > 7)){
+                return 0;
+            }
+            if (ans < Integer.MIN_VALUE/10 || (ans == Integer.MIN_VALUE/10 && pop < -8)){
+                return 0;
+            }
+            ans = ans * 10 + pop;
+        }
+        return ans;
+        
+    }
+}
